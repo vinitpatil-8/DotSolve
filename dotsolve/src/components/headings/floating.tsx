8e6728b6b99b4032a20e425ddf521963d37fb3f3
Vinit <vinit.2007.20@gmail.com>
2026-03-21 18:59:16 +0530

    Made options page responsive

--- dotsolve/src/components/headings/floating.tsx
+++ dotsolve/src/components/headings/floating.tsx
@@ -1,6 +1,6 @@
 const quickselect = (props:any) => {
   return (
-    <div className="w-fit h-fit m-2 text-black font-gaegu cursor-pointer font-bold text-2xl">
+    <div className="w-fit h-fit m-2 text-black font-gaegu cursor-pointer font-bold sm:text-2xl text-xl">
       <div className={props.class}>
         {props.value}
       </div>
--- dotsolve/src/components/pages/options.tsx
+++ dotsolve/src/components/pages/options.tsx
@@ -11,7 +11,7 @@ const options = () => {
     return (
         <div className="full-body w-full min-h-dvh flex flex-col">
             <div className='h-fit w-full'>
-                <div className='w-fit absolute'><Floating class='hover:text-[1.55rem] transition-all duration-300' value='click to go back' /></div>
+                <div className='w-fit absolute'><Floating class='sm:hover:text-[1.55rem] hover:text-[1.30rem] transition-all duration-300' value='click to go back' /></div>
                 <div className='w-full flex justify-center mt-18'><Heading value='Options' /></div>
             </div>
             <div className='flex flex-row w-full h-fit justify-center mt-15'>
@@ -21,7 +21,7 @@ const options = () => {
             <div className='flex flex-col h-fit w-full items-center justify-center mt-4'>
                 <Floating value='Choose your preferred way' />
             </div>
-            <div className='flex flex-row h-fit w-full justify-center gap-45 mt-18'>
+            <div className='flex flex-col sm:flex-row h-fit w-full items-center sm:justify-center gap-20 sm:gap-45 mt-18'>
                 <div data-tooltip-id={"my-tooltip"} data-tooltip-content={"Manually Enter The Grid"}>
 
                     <Button value='Manual' />
